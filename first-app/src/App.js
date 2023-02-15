febc302abed0bbb5df158fe0820aaf853132458f
kdn00 <do7829@naver.com>
2023-02-15 14:08:43 +0900

    react 따라하기

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://github.com/kdn00/React_JS_study"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React 공부 시작!
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        Hello ReactJs!
+      </div>
+    )
+  }
 }
 
 export default App;
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // React 모듈
+import ReactDOM from 'react-dom'; // React-Dom 모듈 : React 코드를 실제 HTML로 렌더링하기 위해 사용하는 도구
+import App from './App'; // App.js 파일
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root')); // id가 root인 element를 가져온다.
+// render 함수 : 첫번째 인자에 담기는 파일(여기서는 App.js)을 불러와서
+//              두 번째 인자로 담기는 HTML element(id='root'인 div)내에 React 코드를 렌더링 시키는 동작을 합니다.
