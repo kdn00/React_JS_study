2e0b0897f123807c957090fe7c80dcc9dd682e98
kdn00 <do7829@naver.com>
2023-02-15 17:16:50 +0900

    React 시작하기

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,3 +1,4 @@
+// React 컴포넌트를 만들 때는 아래의 import문을 통해 React를 불러와야 한다.
 import React, { Component } from 'react';
 import './App.css';
 
@@ -10,5 +11,5 @@ class App extends Component{
     )
   }
 }
-
+// App라는 컴포넌트를 내보내겠다는 의미다. 이렇게 해준다면, 다른 컴포넌트에서 불러와서 사용할 수 있다.
 export default App;
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react'; // React 모듈
-import ReactDOM from 'react-dom'; // React-Dom 모듈 : React 코드를 실제 HTML로 렌더링하기 위해 사용하는 도구
-import App from './App'; // App.js 파일
+// React 모듈
+import React from 'react'; 
+// React-Dom 모듈 : React 코드를 실제 HTML로 렌더링하기 위해 사용하는 도구
+import ReactDOM from 'react-dom';
+// App.js 파일 
+import App from './App'; 
 
-ReactDOM.render(<App />, document.getElementById('root')); // id가 root인 element를 가져온다.
+ReactDOM.render(<App />, document.getElementById('root')); 
+// id가 root인 element를 가져온다.
 // render 함수 : 첫번째 인자에 담기는 파일(여기서는 App.js)을 불러와서
 //              두 번째 인자로 담기는 HTML element(id='root'인 div)내에 React 코드를 렌더링 시키는 동작을 합니다.
