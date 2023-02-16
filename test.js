c6126f5a8e5c1e4b1a98d5328b2d1889e8eab85d
kdn00 <do7829@naver.com>
2023-02-16 11:13:42 +0900

    map

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-// 괄호 안에 이상한 문법이 들어가 버렸습니다...!
-const newFunctionWrong = () => { a: '나는객체요소' };
-
-// 소괄호로 감싸게 되면 객체 자체를 리턴할 수가 있게 됩니다.
-const newFunction = () => (a, { a: '나는객체요소' });
-
-console.log(newFunction());
+const a = [1, 3, 5, 7, 9];
+const aLoop = a.map((unit, idx) => {
+    console.log(unit);
+    return unit+2;
+});
+console.log(aLoop);
